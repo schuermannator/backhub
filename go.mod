ef671c282ed0bf433688e415e245eb42e3b3b48c
Zach Schuermann <zachary.zvs@gmail.com>
2021-03-22 22:17:35 -0400

    refactor

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module github.com/schuermannator/backhub
+module github.com/schuermannator/ghb
 
 go 1.12
 
 require (
+	github.com/cheggaaa/pb/v3 v3.0.7
 	github.com/shurcooL/githubv4 v0.0.0-20191127044304-8f68eb5628d0
 	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
--- main.go
+++ main.go
@@ -1,153 +1,225 @@
 package main
 
 import (
-    "context"
-    "log"
-    "os"
-    "os/exec"
-    "strconv"
-    "strings"
-    "sync"
-    "time"
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 
-    "github.com/shurcooL/githubv4"
-    "golang.org/x/oauth2"
+	"github.com/cheggaaa/pb/v3"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
 )
 
 var timeNow int64 = time.Now().UTC().Unix()
 
 func main() {
-    // parse args
-    if len(os.Args) < 2 {
-        log.Println("usage: os.Args[0] <backup location path>")
-        return
-    }
+	doStars := flag.Bool("s", false, "(stars) save user's stars instead of their own repositories")
+	doAll := flag.Bool("a", false, "(all) save owned repositories and starred")
+	beQuiet := flag.Bool("q", false, "(quiet) be quiet. no progress bar (better for non-interactive use)")
 
-    repos, username := getRepos()	
-    backup := os.Args[1] + "/" + "backup"+strconv.FormatInt(timeNow, 10) + "/"
-    log.Printf("Backing up to %s\n", backup)
-    // create backup dir
-    os.Mkdir(backup, os.ModePerm)
-    log.Printf("Detected %d repositories.\n", len(repos))
-    var wg sync.WaitGroup
-    for name, url := range repos {
-        wg.Add(1)
-        url = strings.Replace(url,"https://", "https://"+username+":"+os.Getenv("GITHUB_TOKEN")+"@", 1)
-        go archive(name, url, &wg)
-    }
-    wg.Wait()
-    log.Println("DONE")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		log.Printf("usage: %s [-qsah] <backup_location_path> <github_acces_token>\n", os.Args[0])
+		return
+	}
+	archive_path := args[0]
+	token := args[1]
+
+	if *doStars && *doAll {
+		log.Fatal("-s and -a specified is ambiguous. Did you mean to save stars (-s) or stars and owned repositories (-a)?")
+	}
+
+	// create backup dir
+	backup := archive_path + "/" + "backup" + strconv.FormatInt(timeNow, 10) + "/"
+	log.Printf("Backing up to %s\n", backup)
+	os.Mkdir(backup, os.ModePerm)
+
+	if !*doStars {
+		// get user's repos
+		repos, username := getRepos(token)
+		log.Printf("Detected %d repositories for %s.\n", len(repos), username)
+		saveAll(repos, token, username, archive_path, *beQuiet)
+	}
+	if *doStars || *doAll {
+		// get user's stars
+		stars, username := getStars(token)
+		log.Printf("Detected %d stars for %s.\n", len(stars), username)
+		saveAll(stars, token, username, archive_path, *beQuiet)
+	}
 }
 
-func archive(name string, url string, wg *sync.WaitGroup) {
-    defer wg.Done()
-
-    archive_path := os.Args[1]
-    path := archive_path + "/" + "backup"+strconv.FormatInt(timeNow, 10) + "/" + name + "/"
-
-    //create dir at path
-    os.Mkdir(path, os.ModePerm)
-
-    log.Println(path)
-    log.Println("scraping " + url + "...")
-
-    cmd := exec.Command("git", "clone", url, path)
-    out, err := cmd.CombinedOutput()
-
-    log.Println(string(out))
-    if err != nil {
-        log.Fatal(err)
-    }
+func saveAll(repos map[string]string, token string, username string, archive_path string, beQuiet bool) {
+	maxGo := 200 // avoid having too many files open
+	limiter := make(chan struct{}, maxGo)
+	if beQuiet {
+		// archive user's repos
+		var wg sync.WaitGroup
+		for name, url := range repos {
+			limiter <- struct{}{}
+			wg.Add(1)
+			url = strings.Replace(url, "https://", "https://"+username+":"+token+"@", 1)
+			go archive(name, url, archive_path, &wg, nil, limiter)
+		}
+		wg.Wait()
+	} else {
+		// archive user's repos
+		bar := pb.Simple.Start(len(repos))
+		var wg sync.WaitGroup
+		for name, url := range repos {
+			limiter <- struct{}{}
+			wg.Add(1)
+			url = strings.Replace(url, "https://", "https://"+username+":"+token+"@", 1)
+			go archive(name, url, archive_path, &wg, bar, limiter)
+		}
+		wg.Wait()
+		bar.Finish()
+	}
 }
 
-func getRepos() (map[string]string, string) {
-    src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+func archive(name string, url string, archive_path string, wg *sync.WaitGroup, bar *pb.ProgressBar, limiter <-chan struct{}) {
+	defer wg.Done()
+	defer func() {
+		<-limiter
+	}()
+	if bar != nil {
+		defer bar.Increment()
+	}
+
+	path := archive_path + "/" + "backup" + strconv.FormatInt(timeNow, 10) + "/" + name + "/"
+
+	//create dir at path
+	os.Mkdir(path, os.ModePerm)
+
+	//log.Println(path)
+	//log.Println("scraping " + url + "...")
+
+	cmd := exec.Command("git", "clone", url, path)
+	_, err := cmd.CombinedOutput()
+
+	//log.Println(string(out))
+	if err != nil {
+		log.Printf("Error: could not save %s\n", url)
+		log.Println(err)
+	}
+}
+
+func getRepos(token string) (map[string]string, string) {
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
-
 	client := githubv4.NewClient(httpClient)
 
-    var infoquery struct {
-	    Viewer struct {
-		    Login     githubv4.String
-		    CreatedAt githubv4.DateTime
-	    }
-    }
+	var q struct {
+		Viewer struct {
+			Login        githubv4.String
+			Repositories struct {
+				Nodes []struct {
+					Name githubv4.String
+					//SshUrl githubv4.String
+					Url   githubv4.String
+					Owner struct {
+						//Id    githubv4.ID
+						Login githubv4.String
+					}
+				}
+				PageInfo struct {
+					EndCursor   githubv4.String
+					HasNextPage bool
+				}
+				TotalCount githubv4.Int
+			} `graphql:"repositories(first: 100, after: $cursor)"`
+		}
+	}
 
-    err := client.Query(context.Background(), &infoquery, nil)
-    if err != nil {
-        panic(err)
-    }
+	variables := map[string]interface{}{
+		"cursor": (*githubv4.String)(nil), // Null after argument to get first page.
+	}
 
-    username := string(infoquery.Viewer.Login)
-    log.Println("    Login:", username)
-    log.Println("CreatedAt:", infoquery.Viewer.CreatedAt)
+	repo_set := make(map[string]string)
+	for {
+		err := client.Query(context.Background(), &q, variables)
+		if err != nil {
+			panic(err)
+		}
+		for _, repo := range q.Viewer.Repositories.Nodes {
+			id := genId(repo.Name, repo.Owner.Login)
+			repo_set[id] = string(repo.Url)
+		}
+		log.Printf("added %d/%d repos", len(q.Viewer.Repositories.Nodes), q.Viewer.Repositories.TotalCount)
+		if !q.Viewer.Repositories.PageInfo.HasNextPage {
+			break
+		}
+		variables["cursor"] = githubv4.NewString(q.Viewer.Repositories.PageInfo.EndCursor)
+	}
 
+	return repo_set, string(q.Viewer.Login)
+}
 
-    var repoquery1 struct {
-	    Viewer struct {
-            Name    githubv4.String
-            Repositories struct {
-                Nodes []struct {
-                    Name githubv4.String
-                    //SshUrl githubv4.String
-                    Url githubv4.String
-                    Owner struct {
-                        //Id    githubv4.ID
-                        Login githubv4.String
-                    }
-                }
-            } `graphql:"repositories(last: 100)"`
-	    }
-    }
+func getStars(token string) (map[string]string, string) {
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	client := githubv4.NewClient(httpClient)
 
-    var repoquery2 struct {
-	    Viewer struct {
-            Name    githubv4.String
-            Repositories struct {
-                Nodes []struct {
-                    Name   githubv4.String
-                    //SshUrl githubv4.String
-                    Url githubv4.String
-                    Owner struct {
-                        //Id    githubv4.ID
-                        Login githubv4.String
-                    }
-                }
-            } `graphql:"repositories(first: 100)"`
-	    }
-    }
+	var q struct {
+		Viewer struct {
+			Login               githubv4.String
+			StarredRepositories struct {
+				Nodes []struct {
+					Name githubv4.String
+					//SshUrl githubv4.String
+					Url   githubv4.String
+					Owner struct {
+						//Id    githubv4.ID
+						Login githubv4.String
+					}
+				}
+				IsOverLimit githubv4.Boolean
+				PageInfo    struct {
+					EndCursor   githubv4.String
+					HasNextPage bool
+				}
+				TotalCount githubv4.Int
+			} `graphql:"starredRepositories(first: 100, after: $cursor)"`
+		}
+	}
 
+	variables := map[string]interface{}{
+		"cursor": (*githubv4.String)(nil), // Null after argument to get first page.
+	}
 
-    err = client.Query(context.Background(), &repoquery1, nil)
-    if err != nil {
-        panic(err)
-    }
-    err = client.Query(context.Background(), &repoquery2, nil)
-    if err != nil {
-        panic(err)
-    }
-    repo_set := make(map[string]string)
-    for _, repo := range repoquery1.Viewer.Repositories.Nodes {
-        id := genId(repo.Name, repo.Owner.Login)
-        repo_set[id] = string(repo.Url)
-    }
-    dups := false
-    for _, repo := range repoquery2.Viewer.Repositories.Nodes {
-        id := genId(repo.Name, repo.Owner.Login)
-        if _, ok := repo_set[id]; ok {
-            dups = true
-        } else {
-            repo_set[id] = string(repo.Url)
-        }
-    }
-    if !dups {
-        log.Println("WARNING: Only first/last 100 scraped.")
-    }
-    return repo_set, username
+	star_set := make(map[string]string)
+	for {
+		err := client.Query(context.Background(), &q, variables)
+		if err != nil {
+			panic(err)
+		}
+		for _, repo := range q.Viewer.StarredRepositories.Nodes {
+			id := genId(repo.Name, repo.Owner.Login)
+			star_set[id] = string(repo.Url)
+		}
+		if q.Viewer.StarredRepositories.IsOverLimit {
+			log.Println("[WARNING] Starred repositories is over github limit. Only a truncated list is present.")
+		}
+		log.Printf("added %d/%d stars", len(q.Viewer.StarredRepositories.Nodes), q.Viewer.StarredRepositories.TotalCount)
+		if !q.Viewer.StarredRepositories.PageInfo.HasNextPage {
+			break
+		}
+		variables["cursor"] = githubv4.NewString(q.Viewer.StarredRepositories.PageInfo.EndCursor)
+	}
+
+	return star_set, string(q.Viewer.Login)
 }
 
 func genId(name githubv4.String, login githubv4.String) string {
-    return string(login) + "_" + string(name)
+	return string(login) + "_" + string(name)
 }
